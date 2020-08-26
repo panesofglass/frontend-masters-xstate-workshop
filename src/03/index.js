670df84169735fd2b5cfb782679e8a1051a3dd55
Ryan Riley <ryanriley@live.com>
2020-08-26 12:19:40 -0500

    Complete exercise 3

--- src/03/index.js
+++ src/03/index.js
@@ -1,28 +1,45 @@
 import { createMachine, interpret } from 'xstate';
 
-const elBox = document.querySelector('#box');
+const elBox = document.getElementById('box');
 
 const machine = createMachine({
-  // Create your state machine here
-  // ...
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: {
+        MOUSEDOWN: 'active'
+      }
+    },
+    active: {
+      on: {
+        MOUSEUP: 'inactive'
+      }
+    }
+  }
 });
 
 // Create a service using interpret(...)
-const service = undefined;
+const service = interpret(machine);
 
 // Listen to state transitions and set
 // `elBox.dataset.state` to the state value as before.
 // ...
+service.onTransition((state) => {
+  elBox.dataset.state = state.value;
+});
 
 // Start the service.
 // ...
+service.start()
 
 elBox.addEventListener('mousedown', (event) => {
   // Send a mousedown event
   // ...
+  service.send('MOUSEDOWN');
 });
 
 elBox.addEventListener('mouseup', (event) => {
   // Send a mouseup event
   // ...
+  service.send('MOUSEUP');
 });
